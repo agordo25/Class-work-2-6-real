d0d0a08994da2ef1e31d4e6bd84161b3874e7bab
agordo25 <34726753+agordo25@users.noreply.github.com>
2018-02-06 14:19:52 -0500

    Add files via upload

---
+++ Class work 2:6.py
@@ -0,0 +1,17 @@
+passage_characters= "This is a paragraph from the book"
+print(len(passage_characters))
+
+#calculate the area of a triangle
+
+def triangle_area(base, height):
+    area= (1.0/2)*base*height
+    return area
+
+print(triangle_area(7,4))
+
+#square function
+def square(value):
+    new_value=value**2
+    print(new_value)
+
+square(7)
